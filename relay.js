decfd1b17bfb76bb2295d1c5c924f5e96090c9d7
sheodox <imagamer27@msn.com>
2019-08-02 22:45:59 -0500

    change config format for options in future

--- relay.js
+++ relay.js
@@ -2,11 +2,11 @@ const router = require('express').Router({strict: true}),
     request = require('request'),
     broadcasters = require('./config.json').broadcasters || [];
 
-router.get('/', async (req, res, next) => {
-    res.render('index', {title: 'Cameras', streams: broadcasters.map((ip, index) => index)})
+router.get('/', async (req, res) => {
+    res.render('index', {title: 'Cameras', streams: broadcasters.map((broadcaster, index) => index)})
 });
 
-broadcasters.forEach((ip, index) => {
+broadcasters.forEach(({ip}, index) => {
     router.use(`/broadcaster/${index}/`, async (req, res, next) => {
         console.log(`http://${ip}:8000/${req.path}`);
         req.pipe(request.get(`http://${ip}:8000${req.path}`)).pipe(res);
