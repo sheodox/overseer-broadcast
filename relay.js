ec3c478526a9920f6c003e633fecbba7c650bdff
sheodox <imagamer27@msn.com>
2019-08-02 22:01:21 -0500

    initial commit

---
+++ relay.js
@@ -0,0 +1,15 @@
+const router = require('express').Router({strict: true}),
+    request = require('request'),
+    broadcasters = require('../config.json').broadcasters || [];
+
+router.get('/', async (req, res, next) => {
+    res.render('index', {title: 'Cameras', streams: broadcasters.map((ip, index) => index)})
+});
+
+broadcasters.forEach((ip, index) => {
+    router.use(`/broadcaster/${index}/`, async (req, res, next) => {
+        req.pipe(request.get(`http://${ip + req.path + query}`)).pipe(res);
+    })
+});
+
+module.exports = router;
