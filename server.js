8ff67611c08a2df1cc336fe6cee009af853e26a4
sheodox <imagamer27@msn.com>
2019-08-03 23:34:12 -0500

    don't let cameras be too big to show on mobile without scrolling

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express'),
 
 app.set('view engine', 'pug');
 app.use(require('./relay.js'));
+app.use(express.static('static'));
 
 app.listen(port, () => console.log(`Overseer Broadcast listening on port ${port}`));
 
