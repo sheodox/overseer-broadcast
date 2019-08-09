5523704ad882fc4b557e0c7de41e44448ca2ba9a
sheodox <imagamer27@msn.com>
2019-08-09 09:54:58 -0500

    move fps/quality out to variables

--- broadcast.py
+++ broadcast.py
@@ -3,13 +3,15 @@ import datetime as dt
 import os
 
 recording_time = 10
+fps = 12
+quality = 32
 
-camera = picamera.PiCamera(resolution=(1280, 720), framerate=12)
+camera = picamera.PiCamera(resolution=(1280, 720), framerate=fps)
 stream = picamera.PiCameraCircularIO(camera, seconds=recording_time)
 
 camera.annotate_background = picamera.Color('black')
 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-camera.start_recording(stream, format='h264', quality=32)
+camera.start_recording(stream, format='h264', quality=quality)
 
 while True:
     start = dt.datetime.now()
@@ -20,7 +22,7 @@ while True:
     stream.copy_to('pivideo.h264', seconds=recording_time)
     os.system('touch video/stream.mp4')
     os.system('rm video/stream.mp4')
-    os.system('MP4Box -add pivideo.h264:fps=12 video/stream.mp4')
+    os.system(f'MP4Box -add pivideo.h264:fps={fps} video/stream.mp4')
     os.system('rm pivideo.h264')
 
 camera.stop_recording()
