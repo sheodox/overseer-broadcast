d4cbb3c9ec8653307d2050f41de7c8c5c198d306
sheodox <imagamer27@msn.com>
2019-08-08 22:44:45 -0500

    match fps of recording

--- record.sh
+++ record.sh
@@ -6,7 +6,7 @@ do
         python3 broadcast.py
         rm video/stream.mp4
         # Wrap the raw video with an MP4 container: 
-        MP4Box -add pivideo.h264 video/stream.mp4
+        MP4Box -add pivideo.h264:fps=12 video/stream.mp4
         # Remove the source raw file, leaving the remaining pivideo.mp4 file to play
         rm pivideo.h264
 done
