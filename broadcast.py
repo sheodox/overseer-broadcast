30ee1dc1c879be4f1e4ae8a75baa3f623fb92656
sheodox <imagamer27@msn.com>
2019-08-08 22:56:36 -0500

    lower quality for lower bandwidth

--- broadcast.py
+++ broadcast.py
@@ -5,7 +5,7 @@ camera = picamera.PiCamera(resolution=(1280, 720), framerate=12)
 camera.start_preview()
 camera.annotate_background = picamera.Color('black')
 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-camera.start_recording('pivideo.h264')
+camera.start_recording('pivideo.h264', quality=28)
 start = dt.datetime.now()
 while (dt.datetime.now() - start).seconds < 5:
     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
