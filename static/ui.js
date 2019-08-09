cefd12b2da79ad9af751b6ef5c4f6acf8532563c
sheodox <imagamer27@msn.com>
2019-08-09 12:20:22 -0500

    add a timeout to stop showing video after ten minutes, but showing a button to start streaming again

--- static/ui.js
+++ static/ui.js
@@ -1,6 +1,13 @@
-let sources;
+const activeMaxTime = 1000 * 60 * 10;
+let sources, inactivePrompt,
+    activeEndTime = Date.now() + activeMaxTime;
 //if a video has finished playing, reload with the next video segment
 function checkForFinishedVideos() {
+    //stop checking if they're inactive, to save bandwidth
+    if (activeEndTime < Date.now()) {
+        showInactivePrompt();
+        return;
+    }
     sources.forEach(source => {
         const video = source.parentNode;
         if (video.duration === video.currentTime) {
@@ -8,6 +15,19 @@ function checkForFinishedVideos() {
         }
     });
     requestAnimationFrame(checkForFinishedVideos);
+}
+
+function showInactivePrompt() {
+    inactivePrompt.classList.remove('hidden');
+    const reactivateButton = document.getElementById('confirm-active');
+    function reactivate() {
+        activeEndTime = Date.now() + activeMaxTime;
+        reactivateButton.removeEventListener('click', reactivate);
+        sources.forEach(touchSource);
+        checkForFinishedVideos();
+        inactivePrompt.classList.add('hidden');
+    }
+    reactivateButton.addEventListener('click', reactivate);
 }
 //use a different url each time, save video is served but prevents all caching
 function touchSource(source) {
@@ -26,6 +46,7 @@ function streamError(source) {
 }
 
 window.addEventListener('load', function() {
+    inactivePrompt = document.getElementById('inactive-prompt');
     sources = document.querySelectorAll('source');
     sources.forEach(touchSource);
     checkForFinishedVideos();
