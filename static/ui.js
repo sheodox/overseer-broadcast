b33eaebd940201489ded03ed5f17e7e09137f24b
sheodox <imagamer27@msn.com>
2019-08-08 23:31:55 -0500

    automatically retry streaming if a video fails to load

--- static/ui.js
+++ static/ui.js
@@ -1,19 +1,30 @@
-const sources = document.querySelectorAll('source');
+let sources;
 function checkForFinishedVideos() {
     sources.forEach(source => {
         const video = source.parentNode;
         if (video.duration === video.currentTime) {
-            touchSources(source);
+            touchSource(source);
         }
     });
     requestAnimationFrame(checkForFinishedVideos);
 }
-function touchSources(source) {
+function touchSource(source) {
     const video = source.parentNode;
     source.src = source.src.replace(/stream.*/, '') + `stream-${Date.now()}.mp4`;
     video.load();
     video.play();
 }
 
-sources.forEach(touchSources);
-checkForFinishedVideos();
+function streamError(source) {
+    console.log(`streaming error on ${source}, retrying...`);
+    setTimeout(function() {
+        touchSource(source);
+    }, 500);
+}
+
+window.addEventListener('load', function() {
+    sources = document.querySelectorAll('source');
+    sources.forEach(touchSource);
+    checkForFinishedVideos();
+});
+
