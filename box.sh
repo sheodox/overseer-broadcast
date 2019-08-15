31f7579a1d213d49d08d42e20cebd984112c898e
sheodox <imagamer27@msn.com>
2019-08-14 21:50:26 -0500

    record to dash segments

--- box.sh
+++ box.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 touch video/stream.mp4
 rm video/stream.mp4
-MP4Box -add pivideo.h264:fps=$1 video/stream.mp4
+MP4Box -dash 15000 -add pivideo.h264:fps=$1 video/stream.mp4
 rm pivideo.h264
+curl http://$2:3200/update
--- broadcast.py
+++ broadcast.py
@@ -1,6 +1,18 @@
 import picamera
 import datetime as dt
 import subprocess
+import json
+
+try:
+    file = open('config.json')
+    config = json.load(file)
+    file.close()
+except FileNotFoundError:
+    print('no config.json, make one with these properties:')
+    print('overseer-server: IP/domain name of the overseer server')
+
+print 
+
 
 recording_time = 10
 fps = 10
@@ -13,15 +25,15 @@ stream = picamera.PiCameraCircularIO(camera, seconds=recording_time * 2)
 camera.annotate_background = picamera.Color('black')
 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 camera.start_recording(stream, format='h264', quality=quality)
+start = dt.datetime.now()
 
 while True:
-    start = dt.datetime.now()
-
     while (dt.datetime.now() - start).seconds < recording_time:
         camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         camera.wait_recording(0.2)
+    start = dt.datetime.now()
     stream.copy_to('pivideo.h264', seconds=recording_time)
     
-    subprocess.Popen(['./box.sh', str(fps)])
+    subprocess.Popen(['./box.sh', str(fps), config['overseer-server']])
 
 camera.stop_recording()
