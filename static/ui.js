a5e9a27976034d18e0d7b42f0dccd9151fc0ece0
sheodox <imagamer27@msn.com>
2019-08-22 22:18:10 -0500

    try to get the next segment if an error or timeout happens, also added video stats

--- static/ui.js
+++ static/ui.js
@@ -22,8 +22,11 @@ function showInactivePrompt() {
 }
 
 class VideoStreamer {
-    constructor(videoElement) {
-        this.video = videoElement;
+    constructor(container) {
+        this.start = Date.now();
+        const find = sel => container.querySelector(sel);
+        this.video = find('video');
+        this.stats = find('.stats');
         this.videoId = this.video.getAttribute('data-stream-id');
         this.ms = new MediaSource();
         this.video.src = URL.createObjectURL(this.ms);
@@ -31,7 +34,32 @@ class VideoStreamer {
         this.video.play();
         this.lastBuffer = new Uint8Array([]);
         this.segmentNumber = 0;
+        this.totalBytes = 0;
         this.fetchNextSegment();
+    }
+    getPrettyBytes(bytes) {
+        let size, unit;
+        const kb = 1024,
+            mb = 1024 * kb,
+            gb = 1024 * mb,
+            checkUnit = (base, u) => {
+                if (bytes / base > 1) {
+                    size = bytes / base;
+                    unit = u;
+                    return true;
+                }
+            };
+
+        [[gb, 'GB'], [mb, 'MB'], [kb, 'KB'], [1, 'Bytes']].some(info => checkUnit(...info));
+        
+        return `${size.toFixed(1)} ${unit}`;
+    }
+    updateStats() {
+        const averageSize = this.getPrettyBytes(this.totalBytes / this.segmentNumber),
+            //don't show hourly bandwidth until after a few segments have been requested,
+            //the first segment comes back immediately so we'd otherwise show some absurd estimate
+            hourlyBandwidth = this.segmentNumber > 5 ? `\n${this.getPrettyBytes((this.totalBytes / (Date.now() - this.start)) * 60 * 60 * 1000)}/hr` : '';
+        this.stats.textContent = `${this.segmentNumber} segments\ntotal ${this.getPrettyBytes(this.totalBytes)}\navg ${averageSize}${hourlyBandwidth}`
     }
     sourceOpen() {
         this.buffer = this.ms.addSourceBuffer('video/mp4; codecs="avc1.640028"');
@@ -44,7 +72,7 @@ class VideoStreamer {
             return showInactivePrompt();
         }
         
-        fetchArrayBuffer(`broadcaster/${this.videoId}/stream/segment/${this.segmentNumber++}`)
+        this.fetchArrayBuffer(`broadcaster/${this.videoId}/stream/segment/${this.segmentNumber++}`)
             .then(buffer => {
                 const newBuffer = new Uint8Array(buffer),
                     isDifferent = newBuffer.length !== this.lastBuffer.length || newBuffer.some((num, i) => num !== this.lastBuffer[i]);
@@ -54,30 +82,44 @@ class VideoStreamer {
                     this.buffer.appendBuffer(newBuffer);
                     this.lastBuffer = newBuffer;
                 }
-                
+
                 //queue up next response
+                this.updateStats();
                 this.fetchNextSegment();
             })
+            .catch(() => {
+                this.fetchNextSegment()
+            })
     }
-}
-function fetchArrayBuffer(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('get', url);
-        xhr.responseType = 'arraybuffer';
-        xhr.onload = function() {
-            if (xhr.status !== 200) {
-                return reject();
-            }
-            resolve(xhr.response);
-        };
-        xhr.send();
-    })
+    fetchArrayBuffer(url) {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('get', url);
+            xhr.responseType = 'arraybuffer';
+            //segments are around 10 seconds, just give it a little more time than it should need to load
+            xhr.timeout = 16000;
+            xhr.onload = evt => {
+                if (xhr.status !== 200) {
+                    return reject();
+                }
+                this.totalBytes += evt.loaded;
+                resolve(xhr.response);
+            };
+            xhr.ontimeout = reject;
+            //prevent flooding the server with requests
+            xhr.onerror = () => {
+                setTimeout(() => {
+                    reject();
+                }, 500);
+            };
+            xhr.send();
+        })
+    }
 }
 
 window.addEventListener('load', function() {
     inactivePrompt = document.getElementById('inactive-prompt');
-    videos = [].slice.call(document.querySelectorAll('video'));
+    videos = [].slice.call(document.querySelectorAll('.stream-container'));
     streamers = videos.map(video => new VideoStreamer(video));
 });
 
