f99ff14f1b0e9009e7ed6415564e528b2f496c2a
sheodox <imagamer27@msn.com>
2019-08-30 22:49:28 -0500

    move broadcaster files. request a key frame after resetting the start time so hopefully it has a less chance of cutting a large amount of time

--- broadcaster-src/broadcast.py
+++ broadcaster-src/broadcast.py
@@ -31,12 +31,12 @@ start = dt.datetime.now()
 while True:
     while (dt.datetime.now() - start).seconds < recording_time:
         camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        camera.wait_recording(0.2)
+        camera.wait_recording(0.1)
     stream.copy_to(os.path.join(config['save-directory'], 'pivideo.h264'), seconds=recording_time)
     # requesting a keyframe now will mean it will have a key frame to split the video at,
     # instead of having to throw away a section of video until the first keyframe for the next segment
-    camera.request_key_frame()
     start = dt.datetime.now()
+    camera.request_key_frame()
     
     subprocess.Popen(['./box.sh', str(fps), config['overseer-server'], config['save-directory']])
 
