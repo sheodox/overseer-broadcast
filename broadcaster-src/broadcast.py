f573c97add0f36362945b1d000148661e60d809c
sheodox <imagamer27@msn.com>
2019-09-05 22:34:38 -0500

    broadcast.py

--- broadcaster-src/broadcast.py
+++ broadcaster-src/broadcast.py
@@ -27,10 +27,10 @@ camera.annotate_background = picamera.Color('black')
 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 camera.start_recording(stream, format='h264', quality=quality)
 start = dt.datetime.now()
-
+camera.exposure_mode = 'fireworks'
 while True:
     while (dt.datetime.now() - start).seconds < recording_time:
-        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
         camera.wait_recording(0.1)
     stream.copy_to(os.path.join(config['save-directory'], 'pivideo.h264'), seconds=recording_time)
     # requesting a keyframe now will mean it will have a key frame to split the video at,
