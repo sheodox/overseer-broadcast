ca09c73e6e417128404dde151d2bef80442e0997
sheodox <3468630+sheodox@users.noreply.github.com>
2020-02-04 21:34:41 -0600

    don't show scrollbars when the screensaver is engaged

--- static/src/Screensaver.js
+++ static/src/Screensaver.js
@@ -2,17 +2,25 @@ import React from 'react';
 import Clock from "./dashboard/Clock";
 import Weather from "./dashboard/Weather";
 
-function Screensaver(props) {
+class Screensaver extends React.Component {
+	componentDidMount() {
+		document.documentElement.classList.add('noscroll');
+	}
+	componentWillUnmount() {
+		document.documentElement.classList.remove('noscroll');
+	}
+	render() {
 
-	return (
-		<div id="screensaver" className="horizontal">
-			<div id="screensaver-info" className="vertical">
-				<Clock />
-				<Weather mode="minimal" />
-				<p>Tap the screen</p>
+		return (
+			<div id="screensaver" className="horizontal">
+				<div id="screensaver-info" className="vertical">
+					<Clock/>
+					<Weather mode="minimal"/>
+					<p>Tap the screen</p>
+				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Screensaver;
