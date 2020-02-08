46a175b2e1523d55a19d73abc51cf86547d11776
sheodox <3468630+sheodox@users.noreply.github.com>
2020-02-08 09:25:20 -0600

    try and fix weather alerts that are missing line breaks.

--- static/src/dashboard/Alerts.js
+++ static/src/dashboard/Alerts.js
@@ -4,9 +4,7 @@ function Alerts(props) {
 	const alerts = (props.alerts || []).map(alert => {
 		return <div className="weather-alert" key={alert.uri}>
 			<h3>{alert.title}</h3>
-			<pre>
-				{alert.description}
-			</pre>
+			<AlertDescription description={alert.description} />
 		</div>
 	});
 	return (
@@ -14,6 +12,37 @@ function Alerts(props) {
 			<button onClick={props.closeAlerts}>Close</button>
 			{alerts}
 		</div>
+	)
+}
+
+/**
+ * Sometimes we get descriptions without line breaks, we need to guess where they should be.
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+function AlertDescription({description}) {
+	//if we don't have more than an arbitrarily small amount of newlines, insert them ourselves
+	const nlMatch = description.match(/\n/g);
+	if (!nlMatch || nlMatch.length < 3) {
+		return (
+			<p className="alert-description">
+				{
+					description.split('* ').map((text, index) => {
+						//don't want to add additional newlines before the first thing
+						if (index === 0) {
+							return text;
+						}
+						return [<br key={`d-${index}-0`}/>, <br key={`d-${index}-1`}/>, `* ${text}`];
+					})
+				}
+			</p>
+		)
+	}
+	return (
+		<pre className="alert-description">
+			{description}
+		</pre>
 	)
 }
 
