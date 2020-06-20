62ba2f9d31e9e210e743f6a176992cf6e7a8ea3b
sheodox <3468630+sheodox@users.noreply.github.com>
2020-06-20 10:44:54 -0500

    Get broadcast working in a docker container.
    Switched the base image to ubuntu instead of python3-buster because I was having issues
    with it. They were probably network issues on my end but this is also a smaller base
    image so that is probably better anyway. The rest of the work was just errors or missing
    binds here and there to get things working. Some file paths were wrong in several places
    and I needed additional bind mounts for things like the right time zone, otherwise the
    time annotated on the video would be in UTC.

--- broadcaster-src/README.md
+++ broadcaster-src/README.md
@@ -1,5 +1,8 @@
-1. Install docker and docker-compose
-2. Setup the camera to be accessible from within docker
- (Instructions originally from https://www.losant.com/blog/how-to-access-the-raspberry-pi-camera-in-docker)
- create a file at /etc/udev/rules.d/99-camera.rules with the text: SUBSYSTEM=="vchiq",MODE="0666"
-3. ./start_broadcast.sh
+# Camera Pi Setup
+1. Install `docker` and `docker-compose`
+2. Setup the camera to be accessible from within docker:
+ create a file at /etc/udev/rules.d/99-camera.rules with the text: `SUBSYSTEM=="vchiq",MODE="0666"`
+ ([Instructions from here](https://www.losant.com/blog/how-to-access-the-raspberry-pi-camera-in-docker))
+3. Create a .env file with an `OVERSEER_SERVER` key which is the IP/host of the Overseer server, the machine
+ that hosts the website.
+4. From within this folder run `./start_broadcast.sh`
--- broadcaster-src/broadcast.py
+++ broadcaster-src/broadcast.py
@@ -1,8 +1,11 @@
 import os
 import picamera
-import datetime as dt
+from datetime import datetime
 import subprocess
 import json
+import sys
+from dotenv import load_dotenv
+load_dotenv()
 
 recording_time = 10
 fps = 10
@@ -10,34 +13,32 @@ quality = 32
 silence_logs = True
 save_directory = '/broadcast-temp'
 
-overseer_server = os.environ['OVERSEER_SERVER']
-if not overseer_server:
-    print("Missing .env with OVERSEER_SERVER defined.")
+try:
+    overseer_server = os.environ['OVERSEER_SERVER']
+except KeyError as e:
+    print(f'Missing .env file with {e.args[0]} defined.')
     sys.exit(1)
 
 print("Overseer Broadcast camera running!")
 
 camera = picamera.PiCamera(resolution=(1280, 720), framerate=fps)
-# buffer more seconds than we actually will save
 stream = picamera.PiCameraCircularIO(camera, seconds=recording_time)
 
 camera.annotate_background = picamera.Color('black')
 camera.start_recording(stream, format='h264', quality=quality)
-start = dt.datetime.now()
+start = datetime.now()
 
 while True:
-    while (dt.datetime.now() - start).seconds < recording_time:
-        camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
+    while (datetime.now() - start).seconds < recording_time:
+        camera.annotate_text = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
         camera.wait_recording(0.1)
     stream.copy_to(os.path.join(save_directory, 'pivideo.h264'), seconds=recording_time)
     # requesting a keyframe now will mean it will have a key frame to split the video at,
     # instead of having to throw away a section of video until the first keyframe for the next segment
-    start = dt.datetime.now()
+    start = datetime.now()
     camera.request_key_frame()
     
-    # ignore all logs, seems we can't just ignore stdout because mp4box logs stdout to stderr,
-    # without this, the forever logs will grow and eventually fill the entire disk
     log_location = subprocess.DEVNULL if silence_logs else None
-    subprocess.Popen(['broadcaster-src/box.sh', str(fps), overseer_server, save_directory], stderr=log_location, stdout=log_location)
+    subprocess.Popen(['./box.sh', str(fps), overseer_server, save_directory], stderr=log_location, stdout=log_location)
 
 camera.stop_recording()
