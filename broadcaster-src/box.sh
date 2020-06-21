53728aad3aefcd25010bc82225c6f8ca915092eb
sheodox <imagamer27@msn.com>
2020-06-20 20:43:15 -0500

    If curl exits with a non-zero exit code retry it.
    This should better handle intermittent network issues, --retry doesn't
    seem to retry if network there's a bad connection. This has a short
    connection timeout and a sleep so it doesn't use up all of the retries
    while network issues are ongoing.

--- broadcaster-src/box.sh
+++ broadcaster-src/box.sh
@@ -2,9 +2,20 @@
 MP4Box -dash 15000 -add $3/pivideo.h264:fps=$1 $3/stream.mp4
 rm $3/pivideo.h264
 
-curl --data-binary "@$3/stream_dashinit.mp4" \
-       	-H "Content-Type: video/mp4" \
-       	--retry 3 \
-	--retry-connrefused \
-	--retry-max-time 7  \
-	http://$2:3200/update
+# attempt to retry if sending the video fails, make up for a flaky connection
+for i in $(seq 1 4); do
+	curl -s --data-binary "@$3/stream_dashinit.mp4" \
+		-H "Content-Type: video/mp4" \
+		--connect-timeout 1 \
+		http://$2:3200/update
+
+	curl_exit_code=$?
+
+	if [ $curl_exit_code -eq 0 ]
+	then
+		exit 0
+	fi
+
+	sleep 1
+	echo "curl exited with code $curl_exit_code, retrying"
+done
