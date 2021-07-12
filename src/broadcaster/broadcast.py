476a72421bd8ffa4b187c330cabd90f81f220adf
sheodox <3468630+sheodox@users.noreply.github.com>
2021-07-12 06:38:38 -0500

    Another attmept at getting 100% video recording. This time instead of doing anything with timing the code to output the video to the file isn't limited by our recording_time, and the stream buffer is emptied as soon as that happens so we start from scratch so the idea is instead of 10 seconds of recording and then saving the last 10 seconds, this will only use the recording time to decide to save all of the current frames to a file. Less dependence on timing should hopefully be more reliable.

--- src/broadcaster/broadcast.py
+++ src/broadcaster/broadcast.py
@@ -24,15 +24,6 @@ print("Overseer Broadcast camera running!")
 
 camera = picamera.PiCamera(resolution=(1280, 720), framerate=fps)
 # let it store a tiny bit more than we want to save in a clip
-# (this is unconfirmed yet, but I'm assuming that since the recording
-# loop waits until it exceeds the recorded_time to save a clip it might
-# be letting the keyframe at the beginning of the clip get cut off and
-# the clip in turn could be getting cut off a bit. attempting to give it
-# a little bit of extra wiggle room so we should always have 100% of the
-# video data we want to be storing. if this assumption is incorrect
-# then we can try to clear the frame buffer between clips and try and save
-# more than the elapsed duration to try and save 100% of the frames without
-# trying to just get there by timing it)
 stream = picamera.PiCameraCircularIO(camera, seconds=recording_time + 5)
 
 camera.annotate_background = picamera.Color('black')
@@ -43,10 +34,15 @@ while True:
     while (datetime.now() - start).seconds < recording_time:
         camera.annotate_text = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
         camera.wait_recording(0.1)
-    stream.copy_to(os.path.join(save_directory, 'pivideo.h264'), seconds=recording_time)
+    stream.copy_to(os.path.join(save_directory, 'pivideo.h264'))
+    # reset the stream buffer since we've saved everything we've already captured.
+    # since we're not specifying a 'seconds' property on copy_to it'll capture
+    # everything we've recorded, since the video until now has been saved, we want
+    # this to start from scratch so it doesn't repeat anything!
+    stream.clear()
+    start = datetime.now()
     # requesting a keyframe now will mean it will have a key frame to split the video at,
     # instead of having to throw away a section of video until the first keyframe for the next segment
-    start = datetime.now()
     camera.request_key_frame()
     
     log_location = subprocess.DEVNULL if silence_logs else None
